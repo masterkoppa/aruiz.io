f2d1431ce6226fe8b3761aef181977cc66c6cab0
Andres Ruiz <andresruiz2010@gmail.com>
2018-12-22 19:49:11 -0500

    Add support for class properties

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,7 @@ var config = {
                 loader: "babel-loader",
                 options: {
                     presets: ["@babel/preset-env", "@babel/preset-react"],
+                    plugins: ["@babel/plugin-proposal-class-properties"]
                 }
             }]}
         ]
